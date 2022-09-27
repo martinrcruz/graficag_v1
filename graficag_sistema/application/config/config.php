717c6e575aaf6c90a6c579550ebfaa291239fab0
martin.rcruz <mrios@sixbell.com>
2022-09-27 09:50:49 -0300

    to production

--- graficag_sistema/application/config/config.php
+++ graficag_sistema/application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://localhost:8089/graficag_v1/graficag_sistema/";
-
+// $config['base_url'] = "http://localhost:8089/graficag_v1/graficag_sistema/";//local
+$config['base_url'] = "localhttps://graficag-api-xnro9.ondigitalocean.app/";
 /*
 |--------------------------------------------------------------------------
 | Index File
